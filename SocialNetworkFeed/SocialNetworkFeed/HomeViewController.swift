9d5402f517f5ebf52e235112c5dc7039fefb242d
Mikhail Ustyantsev <ustyantsevma@gmail.com>
2025-03-05 21:56:26 +0500

    create xCode project

---
+++ SocialNetworkFeed/SocialNetworkFeed/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  SocialNetworkFeed
+//
+//  Created by Mikhail Ustyantsev on 05.03.2025.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+    }
+
+
+}
+
